4c1e76150a91d562b1507451a70a4eac065c9c47
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-05-12 02:52:26 +0600

    adding new users in database

--- index.js
+++ index.js
@@ -23,13 +23,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+    const userCollection = client.db('jobWander').collection('user');
+
+    // user api
+    app.get('/user', async(req, res) => {
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/user', async(req, res) => {
+        const newUser = req.body;
+        console.log(newUser);
+        const result = await userCollection.insertOne(newUser)
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
